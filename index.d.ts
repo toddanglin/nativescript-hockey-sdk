c9196804fb9ad04d5a554bb16b038d85aa99df68
Todd Anglin <toddanglin@gmail.com>
2017-04-10 17:28:14 -0500

    Added explanation about ios/android export (for clarity)

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,8 @@
-export * from './hockeyapp.android'
+/**
+ * iOS and Android apis should match.
+ * It doesn't matter if you export `.ios` or `.android`, either one but ONLY ONE.
+ */
+export * from './hockeyapp.android';
+
+// Export any shared classes, constants, etc.
 export * from './hockeyapp.common';
